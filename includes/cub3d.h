980d6692bfe9c492f934bd87b08aed46d0a3c2df
liulm <liulm@student.42mulhouse.fr>
2025-09-09 15:35:07 +0200

    started cub3d

---
+++ includes/cub3d.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: liulm <liulm@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/09 15:21:42 by liulm             #+#    #+#             */
+/*   Updated: 2025/09/09 15:29:39 by liulm            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include "../utils/minilibx-linux/mlx.h"
+# include "../utils/libft/libft.h"
+# include <stdio.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <sys/time.h>
+
+// ============================ ASSETS ===============================
+
+# define NORTH 'N'
+# define SOUTH 'S'
+# define EAST 'E'
+# define WEST 'W'
+# define FLOOR '0'
+# define WALL '1'
+
+#endif
